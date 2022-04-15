c22e4c0719f23f4692cad1d1441226c097abc570
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-04-14 18:50:30 -0700

    Created some files for front end

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// use public folder for static content
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>  {

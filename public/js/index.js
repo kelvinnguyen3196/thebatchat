dbc609368cd60e20f48bfd64ef004eff2e1a0978
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-04-20 01:51:16 -0700

    Added some comments

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 // immediately focus input when page loads
 document.getElementById(`name`).focus();
 
-// 
+// add event listener for when user presses enter
 document.getElementById(`name`).addEventListener('keydown', function(event) {
-    if(event.key === `Enter`) {
-        
+    if(event.key === `Enter`) { // after pressing enter send to server
+
     }
 });

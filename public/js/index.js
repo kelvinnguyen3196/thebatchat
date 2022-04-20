9130d46aa82904039eb7d65f3907cec33f1ae5a2
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-04-20 14:28:48 -0700

    Started trying to use textarea for input
    There is still a weird behavior where the text area will automatically
    have a new line when you press enter even if you replace it with ' '

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,36 @@
+// get reference to input element
+const input = document.getElementById(`name`);
 // immediately focus input when page loads
-document.getElementById(`name`).focus();
+input.focus();
+// shrink text area input to 1 row
+input.value = ` `;
+input.style.height = 0;
+input.style.height = input.scrollHeight + `px`;
+input.value = ``;
+
+const test = function() {
+    input.focus();
+}
 
 // add event listener for when user presses enter
 document.getElementById(`name`).addEventListener('keydown', function(event) {
     if(event.key === `Enter`) { // after pressing enter send to server
-
+        // remove focus immediately to stop text area from resizing
+        input.blur();
+        // format name to enter into DOM
+        const nameFormatted = `<p>> ${input.value}</p>`;   
+        // insert what user entered before input in DOM
+        document.getElementById(`name-input`).insertAdjacentHTML(`beforebegin`, nameFormatted);
+        // scroll input into view
+        input.scrollIntoView(false);
+        // clear name
+        input.value = ``;
+        test();
     }
+});
+
+// add event listener for auto-adjusting height of input
+document.getElementById(`name`).addEventListener(`input`, function() {
+    this.style.height = 0;
+    this.style.height = this.scrollHeight + `px`;
 });

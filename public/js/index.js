6c60fafdb09931938a9830ffaa02efd19c4d1499
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-04-20 14:32:35 -0700

    Name enters will act like a terminal now

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,18 @@
+// get reference to input element
+const input = document.getElementById(`name`);
 // immediately focus input when page loads
-document.getElementById(`name`).focus();
+input.focus();
 
 // add event listener for when user presses enter
-document.getElementById(`name`).addEventListener('keydown', function(event) {
+input.addEventListener('keydown', function(event) {
     if(event.key === `Enter`) { // after pressing enter send to server
-
+        // format name to enter into DOM
+        const nameFormatted = `<p>> ${input.value}</p>`;   
+        // insert what user entered before input in DOM
+        document.getElementById(`name-input`).insertAdjacentHTML(`beforebegin`, nameFormatted);
+        // scroll input into view
+        input.scrollIntoView(false);
+        // clear name
+        input.value = ``;
     }
 });

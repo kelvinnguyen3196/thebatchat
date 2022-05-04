0c8307f08d5741a2aad2236f97510a7ba461637a
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-05-03 19:09:58 -0700

    Moved a function for organization

--- public/js/room.js
+++ public/js/room.js
@@ -13,6 +13,25 @@ const formattedName = (name) => {
         return name.padEnd(6);
     }
 }
+
+const roomSetup = (roomObj) => {
+    // set expiration date
+    const expirationDate = new Date(roomObj.expiration);
+    const expirationFormatted = dateHelper.dateDifference(new Date(), expirationDate);
+    const date = `${expirationFormatted}`;
+    document.getElementById(`remaining-time`).insertAdjacentHTML(`beforeend`, date);
+
+    console.log(roomObj);
+    
+    const messagesContainer = document.getElementById(`message-container`);
+    roomObj.messages.forEach((elem) => {
+        const message = `${formattedName(elem.name)} > ${elem.message}`;
+        console.log(formattedName(elem.name) + 'here');
+        const msgElem = document.createElement(`p`);
+        msgElem.textContent = message;
+        messagesContainer.insertAdjacentElement(`beforeend`, msgElem);
+    });
+}
 // #endregion
 
 // get reference to message element and focus
@@ -35,27 +54,6 @@ document.getElementById(`menu-button`).addEventListener(`click`, () => {
     const userName = window.location.href.split(`=`)[1];
     window.location.href = `${siteInfo.url}:${siteInfo.port}/rooms?name=${userName}`;
 });
-
-// #region helper functions
-const roomSetup = (roomObj) => {
-    // set expiration date
-    const expirationDate = new Date(roomObj.expiration);
-    const expirationFormatted = dateHelper.dateDifference(new Date(), expirationDate);
-    const date = `${expirationFormatted}`;
-    document.getElementById(`remaining-time`).insertAdjacentHTML(`beforeend`, date);
-
-    console.log(roomObj);
-    
-    const messagesContainer = document.getElementById(`message-container`);
-    roomObj.messages.forEach((elem) => {
-        const message = `${formattedName(elem.name)} > ${elem.message}`;
-        console.log(formattedName(elem.name) + 'here');
-        const msgElem = document.createElement(`p`);
-        msgElem.textContent = message;
-        messagesContainer.insertAdjacentElement(`beforeend`, msgElem);
-    });
-}
-// #endregion
 
 // immediately invoked function - get all data for specific room
 (async () => {

b5bbf472280f5b2f45ed462aa8c21f01e59d1d81
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2022-05-07 18:19:04 -0700

    Forgot to remove the redirect ones too

--- public/js/index.js
+++ public/js/index.js
@@ -30,8 +30,8 @@ input.addEventListener(`keydown`, function(event) {
         input.scrollIntoView(false);
 
         // redirect to chat room page
-        window.location.href = `${siteInfo.url}/rooms?name=${userName}`;
-        console.log(`${siteInfo.url}/rooms?name=${userName}`);
+        window.location.href = `rooms?name=${userName}`;
+        console.log(`rooms?name=${userName}`);
     }
 });
 
